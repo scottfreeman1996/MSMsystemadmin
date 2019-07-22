1dcc4a4772e1d48afc4e16e22246dd052b737a97
Eleanor61082 <Eleanor61082@GB-LD1LPW10G005.mshome.net>
2019-07-22 16:35:07 +0100

    Deploy Spring Application

---
+++ system_admin-app/src/main/java/com/skillsmap/systemadmin/application/SystemAdminApplication.java
@@ -0,0 +1,34 @@
+package com.skillsmap.systemadmin.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class SystemAdminApplication {
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SystemAdminApplication.class);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		ConfigurableApplicationContext ctx = 
+		SpringApplication.run(SystemAdminApplication.class, args);
+		System.out.print("Hello World");
+	}
+	
+	@Controller
+	class WebController {
+		
+		@GetMapping
+		public String home(){
+			return "index";
+		}
+	}
+	
+	
+
+}
