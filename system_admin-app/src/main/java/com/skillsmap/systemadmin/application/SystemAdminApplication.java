9d9f3b299e314880c3009a7e951a63891440eea8
Eleanor61082 <Eleanor61082@GB-LD1LPW10G005.mshome.net>
2019-07-24 12:06:44 +0100

    Added changes to SystemAdminApplication

--- system_admin-app/src/main/java/com/skillsmap/systemadmin/application/SystemAdminApplication.java
+++ system_admin-app/src/main/java/com/skillsmap/systemadmin/application/SystemAdminApplication.java
@@ -3,13 +3,15 @@ package com.skillsmap.systemadmin.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-public class SystemAdminApplication {
+public class SystemAdminApplication extends SpringBootServletInitializer {
 
+	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(SystemAdminApplication.class);
 	}
