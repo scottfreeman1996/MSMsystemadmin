f107527950eb3224bff62ffd3e999822faeb138c
61082EleanorBound <44974866+61082EleanorBound@users.noreply.github.com>
2019-08-20 15:45:26 +0100

    changes to cross origin in controller class

--- system_admin-app/src/main/java/com/systemadmin/controller/SystemAdminController.java
+++ system_admin-app/src/main/java/com/systemadmin/controller/SystemAdminController.java
@@ -1,10 +1,12 @@
 package com.systemadmin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@CrossOrigin(origins = "https://svcsystemadmins.azurewebsites.net")
 public class SystemAdminController {
 	
 	@RequestMapping("/hello")
