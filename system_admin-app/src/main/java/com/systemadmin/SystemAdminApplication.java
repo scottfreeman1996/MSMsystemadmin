d7a629da75aabb4d1449ff6e3bf62059e8f34d3d
61082EleanorBound <44974866+61082EleanorBound@users.noreply.github.com>
2019-08-20 12:57:41 +0100

    change to packaging structure

--- system_admin-app/src/main/java/com/systemadmin/SystemAdminApplication.java
+++ system_admin-app/src/main/java/com/systemadmin/SystemAdminApplication.java
@@ -1,4 +1,4 @@
-package com.skillsmap.systemadmin;
+package com.systemadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- system_admin-app/src/main/java/com/systemadmin/controller/SystemAdminController.java
+++ system_admin-app/src/main/java/com/systemadmin/controller/SystemAdminController.java
@@ -1,4 +1,4 @@
-package com.skillsmap.systemadmin.controller;
+package com.systemadmin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
